854d56cd1f9c26b096328fcc267a0a2da97cb687
wangzhen <wangzhen@curefun.com>
2019-04-04 15:22:29 +0800

    TabLayout

---
+++ 003_TabLayout/TablayoutTest/app/src/main/java/com/android/tablayouttest/TabAdapter.java
@@ -0,0 +1,68 @@
+package com.android.tablayouttest;
+
+import android.content.Context;
+import android.net.rtp.RtpStream;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+    private Context context;
+
+    public TabAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+
+    public View getTabView(int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.custom_tab, null);
+        TextView tabTextView = view.findViewById(R.id.tabTextView);
+        tabTextView.setText(mFragmentTitleList.get(position));
+        return view;
+    }
+
+    //选中的View
+    public View getSelectedTabView(int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.custom_tab, null);
+        TextView tabTextView = view.findViewById(R.id.tabTextView);
+        tabTextView.setText(mFragmentTitleList.get(position));
+        tabTextView.setTextSize(26); // for big text, increase text size
+        tabTextView.setTextColor(ContextCompat.getColor(context, R.color.yellow));
+        return view;
+    }
+
+}
