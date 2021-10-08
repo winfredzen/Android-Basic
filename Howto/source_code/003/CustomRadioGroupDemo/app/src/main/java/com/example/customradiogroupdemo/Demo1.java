3bada9c17cfe9d5eefed0cd067dd67429088c0a0
王振 <wangzh@szlanyou.com>
2021-10-08 10:45:20 +0800

    自定义RadioGroup

---
+++ Howto/source_code/003/CustomRadioGroupDemo/app/src/main/java/com/example/customradiogroupdemo/Demo1.java
@@ -0,0 +1,14 @@
+package com.example.customradiogroupdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Demo1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo1);
+    }
+}
