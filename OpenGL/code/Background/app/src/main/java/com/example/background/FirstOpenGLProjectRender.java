e22cfa0542809706f4d9dd1d5a28c78fe1091a4f
王振 <wangzh@szlanyou.com>
2021-12-14 17:39:23 +0800

    纯色背景

---
+++ OpenGL/code/Background/app/src/main/java/com/example/background/FirstOpenGLProjectRender.java
@@ -0,0 +1,30 @@
+package com.example.background;
+
+import android.opengl.GLES20;
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * create by wangzhen 2021/12/14
+ */
+public class FirstOpenGLProjectRender implements GLSurfaceView.Renderer {
+
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES30.glClearColor(1.0f, 0.0f,0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES30.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+    }
+}
---
+++ OpenGL/code/Background/app/src/main/java/com/example/background/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.background;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView mGLSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setEGLContextClientVersion(3);
+        mGLSurfaceView.setRenderer(new FirstOpenGLProjectRender());
+        setContentView(mGLSurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+}
