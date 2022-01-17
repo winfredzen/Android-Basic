98914cd58b3cfbeed843129cec508465a1e5944d
王振 <wangzh@szlanyou.com>
2022-01-17 16:20:38 +0800

    个推

---
+++ 第三方/code/Getui_SDK_Demo_AS_maven/app/build/generated/source/buildConfig/debug/com/getui/demo/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.getui.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.wz.app.push";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.1.0";
+  // Field from build type: debug
+  public static final String APPKEY = "CEgJ0gsduy7danGdJtphB5";
+  // Field from build type: debug
+  public static final String MASTERSECRET = "xE6Rj4jws76mzrhExGKXqA";
+  // Field from default config.
+  public static final String SERVER_URL = "https://restapi.getui.com/";
+}
