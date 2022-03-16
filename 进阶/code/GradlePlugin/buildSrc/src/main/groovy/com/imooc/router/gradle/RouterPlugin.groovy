796c6ca1fb0484296ed97bfb56085ad004960273
王振 <wangzh@szlanyou.com>
2022-03-16 15:28:40 +0800

    Gradle插件

---
+++ 进阶/code/GradlePlugin/buildSrc/src/main/groovy/com/imooc/router/gradle/RouterPlugin.groovy
@@ -0,0 +1,13 @@
+package com.imooc.router.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class RouterPlugin implements Plugin<Project> {
+
+    //实现apply方法，注入插件的逻辑
+    @Override
+    void apply(Project project) {
+        println("I am from RouterPlugin, apply from ${project.name}")
+    }
+}
