3187830ad92fe398e632013eea98ec5feee66e2e
王振 <wangzh@szlanyou.com>
2022-07-12 15:02:14 +0800

    Camera2

--- Camera/code/Camera2Basic/app/src/main/java/com/example/android/camera2/basic/Person.kt
+++ Camera/code/Camera2Basic/app/src/main/java/com/example/android/camera2/basic/Person.kt
@@ -10,13 +10,13 @@ open class Person(val name: String, val age: Int) {
     }
 }
 
-var content: String? = "hello"
-fun main() {
-    if (content != null) {
-        printUpperCase()
-    }
-}
-fun printUpperCase() {
-    val upperCase = content.toUpperCase()
-    println(upperCase)
-}
+//var content: String? = "hello"
+//fun main() {
+//    if (content != null) {
+//        printUpperCase()
+//    }
+//}
+//fun printUpperCase() {
+//    val upperCase = content.toUpperCase()
+//    println(upperCase)
+//}
