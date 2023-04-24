3204527a7f68c318c5d31aaef57a8b0691aecba4
王振 <wangzh@szlanyou.com>
2023-04-24 09:03:50 +0800

    NumberPicker

---
+++ OpenSource/code/NumberPickerTest/app/src/main/java/com/wz/numberpickertest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.wz.numberpickertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.NumberPicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NumberPicker mNumberPicker;
+
+    private String[] pickerVals;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNumberPicker = findViewById(R.id.number_picker);
+
+        mNumberPicker.setMaxValue(4);
+        mNumberPicker.setMinValue(0);
+
+//        pickerVals = new String[] {"dog", "cat", "lizard", "turtle", "axolotl"};
+//
+//        mNumberPicker.setDisplayedValues(pickerVals);
+
+    }
+}
